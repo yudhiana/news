6b802d52b69937967759ac59442222edd5990a6d
Yudi Yana <yudi.yana@indonesiaindicator.com>
2019-05-08 23:33:33 +0700

    updated

--- news/spiders/tempo.py
+++ news/spiders/tempo.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
-import codecs
-from datetime import datetime
+from news.lib import to_number_of_month, remove_tabs
 from news.items import NewsItem
+from datetime import datetime
 
 
 class TempoSpider(scrapy.Spider):
@@ -16,15 +16,28 @@ class TempoSpider(scrapy.Spider):
         for url in response.css('#article > div.col > section > ul > li  > div > div > a:nth-child(2)::attr(href)'):
             yield scrapy.Request(url=url.get(), callback=self.parse_detail)
 
+    def date_parse(self, date_string):
+        date_lst = str(date_string).strip().split(' ')
+        month = to_number_of_month(date_lst[2].lower())
+        date_str = '{}/{}/{} {}:00'.format(date_lst[1],
+                                           month,
+                                           date_lst[3],
+                                           date_lst[4])
+        date = datetime.strptime(date_str, '%d/%m/%Y %H:%M:%S')
+        return date
+
     def parse_detail(self, response):
-        item = NewsItem()
-        author_lst = response.css('#article #author ::text').getall()
-        author_lst = [re.sub('[\r\t\n]', '', x).lower() for x in author_lst]
-        author_lst = [x.replace('editor:', '').replace('reporter:', '').replace(' ', '') for x in author_lst]
-        author_lst = [x for x in author_lst if len(x) != 0]
-        item['link'] = response.url
-        item['title'] = response.css('#article h1 ::text').get().replace('\t', '').replace('\r', '').strip()
-        item['author'] = ','.join(author_lst)
-        item['date_post_id'] = response.css('span#date::text').get()
-        item['content'] = ''.join(response.css('#isi ::text').getall())
-        yield item
+        if re.search('.*nasional\.tempo.*|.*bisnis\.tempo.*|.*metro\.tempo.*|.*dunia\.tempo.*', response.url):
+            item = NewsItem()
+            author_lst = response.css('#article #author ::text').getall()
+            author_lst = [re.sub('[\r\t\n]', '', x).lower() for x in author_lst]
+            author_lst = [x.replace('editor:', '').replace('reporter:', '').replace(' ', '') for x in author_lst]
+            author_lst = [x for x in author_lst if len(x) != 0]
+            date_string = response.css('span#date::text').get()
+            item['link'] = response.url
+            item['title'] = response.css('#article h1 ::text').get().replace('\t', '').replace('\r', '').strip()
+            item['author'] = ' - '.join(author_lst).title()
+            item['date_post'] = self.date_parse(date_string)
+            item['date_post_id'] = date_string
+            item['content'] = remove_tabs('\n'.join(response.css('#isi p::text').getall()))
+            yield item
--- news/spiders/tribun.py
+++ news/spiders/tribun.py
@@ -2,43 +2,41 @@
 import scrapy
 from datetime import datetime
 from news.items import NewsItem
-
+from news.lib import to_number_of_month, remove_tabs
 
 class TribunSpider(scrapy.Spider):
     name = 'tribun'
     allowed_domains = ['tribunnews.com']
-    date = datetime.now()
-    year = date.year
-    month = date.month
-    day = date.day
+    date = datetime.now().strftime('%Y-%m-%d')
     start_urls = [
-        'http://www.tribunnews.com/index-news/all?date={}-{}-{}'.format(year, month, day),
+        'http://www.tribunnews.com/index-news/all?date={}'.format(date)
     ]
-    header = {
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'en-US,en;q=0.9,en-GB;q=0.8,en-CA;q=0.7,en-ZA;q=0.6,en-AU;q=0.5,en-NZ;q=0.4,id;q=0.3',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Host': 'www.tribunnews.com',
-        'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
-    }
-
-    def start_requests(self):
-        for url in self.start_urls:
-            print url
-            yield scrapy.Request(url=url, headers=self.header)
 
     def parse(self, response):
         for href in response.xpath('/html/body/div[4]/div[4]/div[1]/div/div[2]/div[2]/ul/li/h3/a/@href'):
             yield scrapy.Request(url=href.get(), callback=self.parse_detail)
+        paging = response.css('.paging a::attr(href)')
+        if paging:
+            number_pg = paging[-1].get()
+            print number_pg
+            
+    def date_parse(self, date_string):
+        date_lst = str(date_string).strip().split(' ')
+        month = to_number_of_month(date_lst[2].lower())
+        date_str = '{}/{}/{} {}:00'.format(date_lst[1],
+                                           month,
+                                           date_lst[3],
+                                           date_lst[4])
+        date = datetime.strptime(date_str, '%d/%m/%Y %H:%M:%S')
+        return date
 
     def parse_detail(self, response):
         item = NewsItem()
+        date_string = response.css('.content time::text').get()
         item['title'] = response.css('.content h1::text').get()
         item['link'] = response.url
-        item['date_post_id'] = response.css('.content time::text').get()
-        item['content'] = ''.join(response.css('.side-article.txt-article ::text').getall())
+        item['author'] = 'TribunNews'
+        item['date_post'] =  self.date_parse(date_string)
+        item['date_post_id'] = date_string
+        item['content'] = remove_tabs('\n'.join(response.css('.side-article.txt-article p::text').getall()))
         yield item
-
