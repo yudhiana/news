e1d40bea80f6a2aee21e5e9dac98aa0d4bbe3309
Yudi Yana <yudi.yana@indonesiaindicator.com>
2019-05-08 21:16:35 +0700

    updated

--- news/spiders/sindo.py
+++ news/spiders/sindo.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+
 import scrapy
 import re
-from datetime import datetime
+from bs4 import BeautifulSoup
 from news.items import NewsItem
+from news.lib import remove_tabs, to_number_of_month
+from datetime import datetime
+
 
 class SindoSpider(scrapy.Spider):
     name = 'sindo'
@@ -29,12 +33,25 @@ class SindoSpider(scrapy.Spider):
                 result.append('https://index.sindonews.com/index/0/{}?t={}'.format(pg, self.date))
         return result
 
+    def date_parse(self, date_string):
+        date_lst = str(date_string).strip().split(' ')
+        month = to_number_of_month(date_lst[2].lower())
+        date_str = '{}/{}/{} {}:00'.format(date_lst[1],
+                                           month,
+                                           date_lst[3],
+                                           date_lst[5])
+        date = datetime.strptime(date_str, '%d/%m/%Y %H:%M:%S')
+        return date
+
+
     def parse_detail(self, response):
         item = NewsItem()
+        date_string = response.css('div.article time::text').get()
         item['title'] = response.css('.article h1::text').get()
         item['link'] = response.url
         item['author'] =  response.css('.article .reporter p a::text').get()
-        item['date_post_id'] = response.css('div.article time::text').get()
-        item['content'] = ''.join(response.css('.article #content ::text').getall())
+        item['date_post'] = self.date_parse(date_string)
+        item['date_post_id'] = date_string
+        item['content'] = remove_tabs('\n'.join(response.css('.article #content::text').getall()))
         return item
 
