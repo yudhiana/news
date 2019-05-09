f12b7165aa3643a1fbbafd86f843a9e1b9b6d860
Yudi Yana <yudi.yana@indonesiaindicator.com>
2019-05-09 07:18:11 +0700

    updated

--- news/spiders/tribun.py
+++ news/spiders/tribun.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+
 import scrapy
+import re
 from datetime import datetime
 from news.items import NewsItem
 from news.lib import to_number_of_month, remove_tabs
@@ -9,17 +11,21 @@ class TribunSpider(scrapy.Spider):
     allowed_domains = ['tribunnews.com']
     date = datetime.now().strftime('%Y-%m-%d')
     start_urls = [
-        'http://www.tribunnews.com/index-news/all?date={}'.format(date)
+        'http://www.tribunnews.com/index-news?date={}'.format(date)
     ]
 
     def parse(self, response):
         for href in response.xpath('/html/body/div[4]/div[4]/div[1]/div/div[2]/div[2]/ul/li/h3/a/@href'):
             yield scrapy.Request(url=href.get(), callback=self.parse_detail)
-        paging = response.css('.paging a::attr(href)')
-        if paging:
-            number_pg = paging[-1].get()
-            print number_pg
-            
+        if not re.search('.*&page=.*', response.url):
+            paging = response.css('.paging a::attr(href)')
+            if paging:
+                number_pg = paging[-1].get()
+                number_pg = re.sub('.*&page=([0-9]+).*','\g<1>', number_pg)
+                for page in range(2, int(number_pg)):
+                    next_page = '{}&page={}'.format(self.start_urls[-1], page)
+                    yield scrapy.Request(next_page, callback=self.parse)
+
     def date_parse(self, date_string):
         date_lst = str(date_string).strip().split(' ')
         month = to_number_of_month(date_lst[2].lower())
