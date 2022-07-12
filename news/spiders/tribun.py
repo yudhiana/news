7b5c1921845de48d328c221e88dfe9fc4846a2d4
yudhiana99 <yudiyana@mbiz.co.id>
2022-07-12 10:51:47 +0700

    Update module tribun

--- news/spiders/tribun.py
+++ news/spiders/tribun.py
@@ -27,8 +27,33 @@ class TribunSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url+"?page=all", callback=self.parse_detail)
 
+    def parse_detail(self, response):
+        item = NewsItem()
+        item['date_post'] = self.get_date(response)
+        item['date_post_local_time'] = self.get_date_post_local_time(response)
+        item['author'] = self.get_author(response)
+        item['title'] = self.get_title(response)
+        item['link'] = response.url
+        item['content'] = self.get_content(response)
+        yield item
+
+    def get_content(self, response):
+        content_lst = response.css(
+            '.side-article.txt-article p::text').getall()
+        if content_lst:
+            content = '\n'.join(content_lst)
+            return content
+        return None
+
+    def get_title(self, response):
+        content = response.css('.content')
+        return content.css('h1::text').get().strip()
+
+    def get_author(self, response):
+        return self.parse_author(response)
+
     def parse_author(self, response):
-        author = ''
+        author = None
         scripts = response.css("script")
         for script in scripts:
             script_type = script.css('::attr(type)').get()
@@ -37,18 +62,15 @@ class TribunSpider(scrapy.Spider):
                     data = loads(script.css("::text").get().replace(
                         "\n", "").replace("\t", "").strip())
                     if 'author' in data:
-                        author = data['author']['name']
+                        author = data['author']['name'].title()
         return author
 
-    def parse_detail(self, response):
+    def get_date_post_local_time(self, response):
         content = response.css('.content')
-        date_str = content.css('#article time ::text').get().strip()
-        item = NewsItem()
-        item['date_post'] = date_parse(date_str)
-        item['date_post_local_time'] = date_str
-        item['author'] = self.parse_author(response)
-        item['title'] = content.css('h1::text').get().strip()
-        item['link'] = response.url
-        item['content'] = '\n'.join(response.css(
-            '.side-article.txt-article p::text').getall())
-        yield item
+        return content.css('#article time ::text').get().strip()
+
+    def get_date(self, response):
+        date = self.get_date_post_local_time(response)
+        if date:
+            return date_parse(date)
+        return None
